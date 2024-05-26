54e4a4bc5b27cab47ce0da70fd293acb76c18f36
SatwikTatikonda <satwk.tatikonda@gmail.com>
2024-05-26 19:59:52 +0530

    26 may push

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -2,6 +2,7 @@ from scipy.spatial import distance
 from imutils import face_utils
 import imutils
 import dlib
+from pygame import mixer
 import cv2
 
 def eye_aspect_ratio(eye):
@@ -10,24 +11,28 @@ def eye_aspect_ratio(eye):
 	C = distance.euclidean(eye[0], eye[3])
 	ear = (A + B) / (2.0 * C)
 	return ear
+
+mixer.init()
+sound = mixer.Sound('alarm.wav')
+
 	
 thresh = 0.25
-frame_check = 20
+frame_check = 5
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
 cap=cv2.VideoCapture(0)
 flag=0
 while True:
-	ret, frame=cap.read()
+	ret,frame=cap.read()
 	frame = imutils.resize(frame, width=450)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	subjects = detect(gray, 0)
 	for subject in subjects:
 		shape = predict(gray, subject)
-		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+		shape = face_utils.shape_to_np(shape)
 		leftEye = shape[lStart:lEnd]
 		rightEye = shape[rStart:rEnd]
 		leftEAR = eye_aspect_ratio(leftEye)
@@ -45,7 +50,8 @@ while True:
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 				cv2.putText(frame, "****************ALERT!****************", (10,325),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				#print ("Drowsy")
+				sound.play()
+
 		else:
 			flag = 0
 	cv2.imshow("Frame", frame)
